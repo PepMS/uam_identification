000422faa0b0cb845a38961ded1624ca72da36bc
PepMS <jmarti@upc.edu>
2021-11-26 05:37:48 +0100

    wip

--- identif_pin_px4.py
+++ identif_pin_px4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 import identification
 import pinocchio
@@ -6,6 +7,21 @@ import crocoddyl
 import eagle_mpc
 
 from eagle_mpc.utils.robots_loader import load
+
+print('Data loading...')
+file_name = '/home/pepms/robotics/method-test/uam_identification/csvs/3_minutes.csv'
+
+prop_min_speed = 100  # Only valid for this simulation
+prop_max_speed = 1100  # Only valid for this simulation
+
+data = identification.multicopterData(file_name, prop_min_speed, prop_max_speed)
+
+rotor_rads, rotor_ts = data.loadRotorAngularVelocity()
+states, state_ts = data.loadPlatformState()
+a_lin_flus, a_lin_ts = data.loadAcceleration()
+a_ang_flus, a_ang_ts = data.loadAngularAcceleration()
+
+print('Robot loading...')
 
 robot = load("iris_px4")
 r_model = robot.model
@@ -23,49 +39,39 @@ W_lst = []
 Wm_lst = []
 Wk_lst = []
 
-for i in range(10000):
+idx_min = 0
+idx_max = 2000
 
-    x = r_state.rand()
-    x[:3] = np.random.rand(3)
+as_model = []
+a_lin_nogs = []
 
-    u = mc_params.u_lb + np.random.rand(mc_params.n_rotors) * (mc_params.u_ub - mc_params.u_lb)
-    n_rotors = np.sqrt(u / mc_params.cf)
-    act_model.calc(act_data, x, u)
+for idx, (rotor, state, a_lin, a_ang) in enumerate(
+        zip(rotor_rads[idx_min:idx_max], states[idx_min:idx_max], a_lin_flus[idx_min:idx_max],
+            a_ang_flus[idx_min:idx_max])):
+
+    q = pinocchio.Quaternion(state[3:7])
+
+    a = a_lin + q.toRotationMatrix().T @ np.array([0, 0, -9.81])
+    a_lin_nogs.append(a)
+
+    v_motion = pinocchio.Motion(state[7:10], state[10:13])
+    a_motion = pinocchio.Motion(a, a_ang)
+
+    thrust = rotor**2 * mc_params.cf
+    act_model.calc(act_data, state, thrust)
     tau = act_data.tau
 
-    accel = pinocchio.aba(r_model, r_data, x[:r_model.nq], x[r_model.nq:], tau)
+    a_model = pinocchio.aba(r_model, r_data, state[:r_model.nq], state[r_model.nq:], tau)
+    as_model.append(a_model[:3])
 
-    # Checking start
-    p = x[:3]
-    q = pinocchio.Quaternion(x[6], x[3], x[4], x[5])
-    R = q.toRotationMatrix()
-    v = x[7:10]
-    w = x[10:]
-    a_lin = accel[:3]
-    a_ang = accel[3:]
+fig, axs = plt.subplots(3)
+axs[0].plot([a[0] for a in a_lin_nogs[idx_min:idx_max]])
+axs[0].plot([a[0] for a in as_model[idx_min:idx_max]])
 
-    v_motion = pinocchio.Motion(v, w)
-    a_motion = pinocchio.Motion(a_lin, a_ang)
+axs[1].plot([a[1] for a in a_lin_nogs[idx_min:idx_max]])
+axs[1].plot([a[1] for a in as_model[idx_min:idx_max]])
 
-    D, Dm = identification.computeDDm(R, v_motion, a_motion)
-    Dk = identification.computeDk(n_rotors)
+axs[2].plot([a[2] for a in a_lin_nogs[idx_min:idx_max]])
+axs[2].plot([a[2] for a in as_model[idx_min:idx_max]])
+plt.show()   
 
-    W_lst.append(D)
-    Wm_lst.append(np.array([Dm]).T)
-    Wk_lst.append(-Dk)
-
-m = 1.52
-Wt = np.concatenate([np.vstack(Wk_lst), np.vstack(W_lst), m * np.vstack(Wm_lst)], axis=1)
-
-identification.runIdentification(Wt)
-
-print("Real parameters: ")
-dyn_param = r_model.inertias[1].toDynamicParameters()
-
-params = ["cf", "cm", "ms_x", "ms_y", "ms_z", "Ixx", "Iyy", "Izz", "Ixy", "Ixz", "Iyz"]
-values = np.array([5.84e-06, 3.504e-7, dyn_param[1], dyn_param[2], dyn_param[3], dyn_param[4], dyn_param[6], dyn_param[9], dyn_param[5], dyn_param[7],
-    dyn_param[8]
-])
-
-for (param, value) in zip(params, values):
-    print(param, ":", value)
